9ed7038ba6e3dc947ce72c7edda785d6a45951b3
dsmiff <dominic.smith@bristol.ac.uk>
2014-10-20 17:18:26 +0200

    tidying up

--- L1Trigger/L1GctAnalyzer/test/CheckErrors.sh
+++ L1Trigger/L1GctAnalyzer/test/CheckErrors.sh
@@ -1,10 +1,23 @@
 INPUTFILE=$1
+errorfile=${INPUTFILE%.*}
+
+
+if [ $# -eq 0 ]
+then
+    echo "No input file"
+    exit 1
+fi  
+
+
+
 
 echo "Input file is $INPUTFILE"
 
-if grep -Fr "GCT fibre data error" $INPUTFILE || grep -Fr "Gct fibre data error" $INPUTFILE
+if grep -Fr "GCT fibre data error" $INPUTFILE -A 1 || grep -Fr "Gct fibre data error" $INPUTFILE -A 1  
 then
 echo "Found a Fibre Data Error"
+grep -Fr "GCT fibre data error" $INPUTFILE -A 1 >> $errorfile.stderr
+grep -Fr "Gct fibre data error" $INPUTFILE -A 1 >> $errorfile.stderr
 else
 echo "No Fibre Data Error Found"
 fi
