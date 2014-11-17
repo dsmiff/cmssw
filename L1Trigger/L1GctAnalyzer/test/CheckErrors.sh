0af140f8704562c01d12a7f14912dd8d554f8a99
dsmiff <dominic.smith@bristol.ac.uk>
2014-11-17 10:33:59 +0100

    script to run over message logger output and check for errors

--- L1Trigger/L1GctAnalyzer/test/CheckErrors.sh
+++ L1Trigger/L1GctAnalyzer/test/CheckErrors.sh
@@ -13,11 +13,13 @@ fi
 
 echo "Input file is $INPUTFILE"
 
-if grep -Fr "GCT fibre data error" $INPUTFILE -A 1 || grep -Fr "Gct fibre data error" $INPUTFILE -A 1  
+if grep -Fr "GCT fibre data error" $INPUTFILE -A 1 || grep -Fr "Gct fibre data error" $INPUTFILE -A 1 || grep -Fr "Fibre size error" $INPUTFILE -A 1 || grep -Fr "Unpacking error" $INPUTFILE -A 1 
 then
 echo "Found a Fibre Data Error"
 grep -Fr "GCT fibre data error" $INPUTFILE -A 1 >> $errorfile.stderr
 grep -Fr "Gct fibre data error" $INPUTFILE -A 1 >> $errorfile.stderr
+grep -Fr "Fibre size error" $INPUTFILE -A 1 >> $errorfile.stderr
+grep -Fr "Unpacking error" $INPUTFILE -A 1 >> $errorfile.stderr
 else
 echo "No Fibre Data Error Found"
 fi
